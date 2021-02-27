ada4d299fbd571664786865557d63f54f1fd4a26
basharalidreesi <69329190+basharalidreesi@users.noreply.github.com>
2021-02-27 15:49:12 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+console.log("Title: " + document.title);
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+console.log("--vh set to " + `${vh}px` + ".");
+
 function lock_scroll() {
 	window.scrollTo(0, 0);
 	document.body.classList.toggle("--lock_scroll");
