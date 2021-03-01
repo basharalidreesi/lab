a30986e59ca45109dfdbbc6de78e76f50dc74da6
Bashar al-Idreesi <basharalidreesi@gmail.com>
2021-03-01 18:58:47 +0200

    Updated Script

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,13 @@ let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 console.log("--vh set to " + `${vh}px` + ".");
 
+let url = window.location.href;
+let trailing_slash = url.endsWith("/");
+if (trailing_slash == true) {
+	let new_url = window.location.href.slice(0, -1);
+	window.location.replace(new_url);
+}
+
 function lock_scroll() {
 	window.scrollTo(0, 0);
 	document.body.classList.toggle("--lock_scroll");
