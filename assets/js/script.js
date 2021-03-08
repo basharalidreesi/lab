74ad5c48be8f80a401f7e4a57a7994ee842f40ff
Bashar al-Idreesi <basharalidreesi@gmail.com>
2021-03-08 14:25:30 +0200

    Updated Default and 4 other files

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,10 @@ let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 console.log("--vh set to " + `${vh}px` + ".");
 
+window.addEventListener("load", loaded);
+window.addEventListener("load", arrowed);
+window.addEventListener("scroll", unarrowed, {passive: true});
+
 // let url = window.location.href;
 // let trailing_slash = url.endsWith("/");
 // if (window.location.pathname != "/" && trailing_slash == true) {
@@ -11,13 +15,45 @@ console.log("--vh set to " + `${vh}px` + ".");
 //	window.location.replace(new_url);
 // }
 
-function lock_scroll() {
-	window.scrollTo(0, 0);
-	document.body.classList.toggle("--lock_scroll");
-	console.log("Body scrolling temporarily locked.");
-}
-
-window.addEventListener("load", loaded);
 function loaded() {
 	console.log("Reticulated splines.");
+}
+
+function arrowed() {
+	if (typeof(document.getElementById("arrow_wrapper")) != "underfined" && document.getElementById("arrow_wrapper") != "null") {
+		document.getElementById("arrow_wrapper").style.display = "block";
+		console.log("Arrowed.");
+	} else {
+		window.removeEventListener("load", arrowed);
+		console.log("No arrows here. Scroll listener removed.");
+	}
+}
+
+function unarrowed() {
+	if (typeof(document.getElementById("arrow_wrapper")) != "underfined" && document.getElementById("arrow_wrapper") != "null") {
+		document.getElementById("arrow_wrapper").style.opacity = "0";
+		console.log("Arrow hid.");
+		window.removeEventListener("scroll", unarrowed, {passive: true});
+		console.log("Scroll listener removed.");
+		setTimeout(() => { document.getElementById("arrow_wrapper").remove() }, 1250);
+		setTimeout(() => { console.log("Unarrowed.") }, 1250);
+	} else {
+		window.removeEventListener("scroll", unarrowed, {passive: true});
+		console.log("No arrows here. Scroll listener removed.");
+	}
+}
+
+function toArrow(event) {
+	var scroll_to = event.target;
+	scroll_to.scrollIntoView({
+		behavior: 'auto',
+		block: 'center',
+		inline: 'center'
+	});
+}
+
+function lock_scroll() {
+	// window.scrollTo(0, 0);
+	// document.body.classList.toggle("--lock_scroll");
+	// console.log("Body scrolling temporarily locked.");
 }
