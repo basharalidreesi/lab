237cfac00f0cd83b119e0736962b9dd969faa6de
bashar <basharalidreesi@gmail.com>
2021-03-11 02:14:52 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -105,7 +105,8 @@ function redirected(event) {
 				document.body.classList.add("--lock_scroll");
 				document.getElementById("cascade_wrapper").scrollLeft += (event.deltaY + event.deltaX);
 			} else if (event.deltaY < 0) { // if going up
-				if (document.getElementById("cascade_wrapper").scrollLeft == 0) {
+				if (document.getElementById("cascade_wrapper").scrollLeft <= 0) {
+					event.preventDefault();
 					document.body.classList.remove("--lock_scroll");
 				} else if (document.getElementById("cascade_wrapper").scrollLeft > 0) {
 					document.getElementById("cascade_wrapper").scrollLeft += (event.deltaY + event.deltaX);
