2eef56f93c50f5a45da8d8ec73b3d3d3abb05c30
Bashar al-Idreesi <basharalidreesi@gmail.com>
2021-03-11 02:02:57 +0200

    Updated Script

--- assets/js/script.js
+++ assets/js/script.js
@@ -111,6 +111,17 @@ function redirected(event) {
 					document.getElementById("cascade_wrapper").scrollLeft += (event.deltaY + event.deltaX);
 				}
 			}
+			if (event.deltaX > 0) { // if going right
+				event.preventDefault();
+				document.body.classList.add("--lock_scroll");
+				document.getElementById("cascade_wrapper").scrollLeft += (event.deltaY + event.deltaX);
+			} else if (event.deltaX < 0) { // if going left
+				if (document.getElementById("cascade_wrapper").scrollLeft == 0) {
+					document.body.classList.remove("--lock_scroll");
+				} else if (document.getElementById("cascade_wrapper").scrollLeft > 0) {
+					document.getElementById("cascade_wrapper").scrollLeft += (event.deltaY + event.deltaX);
+				}
+			}
 		}
 	} else {
 		window.removeEventListener("wheel", redirected);
